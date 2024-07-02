45c37931342567f89f0a5e34f14b99e9ab095dc7
mje208 <mero.john.edward@gmail.com>
2024-07-02 17:30:00 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env/python3 
+
+my_list= ["192.168.0.5", 5060, "up"]
+
+print("The first item in this list (IP):" + my_list[0])
+
+print('The second item in the list (port):' +str(my_list[1]))
+
+print('The last item in the list (state): ' + my_list[2])
+
+iplist = [5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh"]
+
+print(iplist[3:5])
+
