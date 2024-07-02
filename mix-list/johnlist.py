0bb96358212f80b85f5bacea83f53ae4aac604ff
mje208 <mero.john.edward@gmail.com>
2024-07-02 18:00:30 +0000

    mixed lists

---
+++ mix-list/johnlist.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env/python3
+
+
+# list is formatted in a random order, this example shows a strat to rearrange the words for it to make a coherent sentence
+
+johnlist = ['is', 'cheeseburger', 'john', 'eating', 'a', ['it was a good day']]
+
+
+
+print(f' {johnlist[2]} {johnlist[0]} {johnlist[3]} {johnlist[4]} {johnlist[1]} ')
+
+print(f' {johnlist[2]} {johnlist[0]} {johnlist[3]} {johnlist[4]} {johnlist[1]} ,it was a good day ')
+
+
