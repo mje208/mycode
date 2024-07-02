3da970371914ef37cb8868bc2523e359ef08dc76
mje208 <mero.john.edward@gmail.com>
2024-07-02 16:57:21 +0000

    added tuesdaychallenge.py script

---
+++ tuesdaychallenge.py
@@ -0,0 +1,10 @@
+#!/usr/env/python3 
+
+mylist=[1, 2, 3, 4, 5, "Python"]
+name=input("What is your name?\n")
+
+print(f'hi {name.capitalize()}! Welcome to Day {mylist[1]} of {mylist[5]} Training!')
+
+
+
+
