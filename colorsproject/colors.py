e0251af0101231b762dfea22c51530e138b9d203
mje208 <mero.john.edward@gmail.com>
2024-07-05 15:28:40 +0000

    changes made; put hard code into a function

--- colorsproject/colors.py
+++ colorsproject/colors.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env/python3
 
+def main():
 
-favcolor = input('what is your favorite color? \n')
-
-
-if favcolor == 'blue' or favcolor == 'red' or favcolor == 'green':
-    print('that is a great color choice. truly inspiring!\n')
-
-elif favcolor == 'purple' or favcolor == 'pink' or favcolor == 'burgundy' or favcolor == 'orange':
-    print('this is a hideous colors. try again.')
-
-elif favcolor in ['1','2','3','4','5','6']:
-    print('that is not a color, you silly goose.')
-
-else:
-    print('that is not a good color. you will now be swatted')
+    favcolor = input('what is your favorite color? \n')
 
 
+    if favcolor == 'blue' or favcolor == 'red' or favcolor == 'green':
+        print('that is a great color choice. truly inspiring!\n')
+
+    elif favcolor == 'purple' or favcolor == 'pink' or favcolor == 'burgundy' or favcolor == 'orange':
+        print('this is a hideous colors. try again.')
+
+    elif favcolor in ['1','2','3','4','5','6']:
+        print('that is not a color, you silly goose.')
+
+    else:
+        print('that is not a good color. you will now be swatted')
+
+main()
